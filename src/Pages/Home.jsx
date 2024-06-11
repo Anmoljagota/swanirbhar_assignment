9ae5fedaad408f109aa2408bd03dfb30efa81349
Anmoljagota <anmoljagota08@gmail.com>
2024-06-11 11:29:26 +0530

    filtering removed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -80,11 +80,6 @@ const Home = () => {
               mr={4}
               onChange={handleInput}
             />
-            <Select placeholder="Filter by" maxW="200px">
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </Select>
           </Flex>
         </Flex>
         {loading ? (
